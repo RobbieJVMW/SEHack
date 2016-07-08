1f0fad01d0a20fc2462e43cb24a74df9c281195e
andy7t@gmail.com <andrew@arnold.home>
2016-07-08 14:59:28 +0100

    With graphs

--- webserver/index.php
+++ webserver/index.php
@@ -84,11 +84,57 @@ switch ($input_function) {
         <TD>$row[value]</TD>
 
         </TR>"; 
-	  
+	       $labels.="\"$row[datetime]\","; 
+           $datapoints.="\"$row[value]\",";       
 	       }
 	   }
 	   else {echo "<TR><TD>Null</TD></TR>"; }
-	   
-	   return $response;
-	}
+	   echo "</TABLE>";
+        
+        ?>
+    
+ 
+<script src="Chart.js"></script>
+<canvas id="myChart" width="200" height="200"></canvas>
+
+<script>
+    
+    var data = {
+    labels: [ <?php echo "$labels"; ?> ],
+    datasets: [
+        {
+            label: "<?php echo "$type" ?> ",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [ <?php echo "$datapoints"; ?>],
+        }
+    ]
+};
+    
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: data
+});
+</script>
+
+
+    
+    <?php
+    }
 ?>
